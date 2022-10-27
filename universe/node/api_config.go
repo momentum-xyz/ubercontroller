2f4568cf3467276d266f172a352ace9d27ed4057
Dmitry Zinovyev <terbooter@gmail.com>
2022-10-27 13:41:39 +0300

    NEX-1 add swagger docs for /api/v4/config/ui-client

--- universe/node/api_config.go
+++ universe/node/api_config.go
@@ -8,8 +8,28 @@ import (
 	"github.com/momentum-xyz/ubercontroller/config"
 )
 
+// @Summary Config for UI client
+// @Schemes
+// @Description Returns config for UI client
+// @Tags config
+// @Accept json
+// @Produce json
+// @Success 200 {object} node.apiGetUIClientConfig.Response
+// @Success 500 {object} api.HTTPError
+// @Router /api/v4/config/ui-client [get]
 func (n *Node) apiGetUIClientConfig(c *gin.Context) {
 	unityClientURL := n.cfg.UIClient.FrontendURL + "/unity"
+
+	type Response struct {
+		config.UIClient
+		UnityClientURL          string `json:"UNITY_CLIENT_URL"`
+		UnityClientLoaderURL    string `json:"UNITY_CLIENT_LOADER_URL"`
+		UnityClientDataURL      string `json:"UNITY_CLIENT_DATA_URL"`
+		UnityClientFrameworkURL string `json:"UNITY_CLIENT_FRAMEWORK_URL"`
+		UnityClientCodeURL      string `json:"UNITY_CLIENT_CODE_URL"`
+		RenderServiceURL        string `json:"RENDER_SERVICE_URL"`
+		BackendEndpointURL      string `json:"BACKEND_ENDPOINT_URL"`
+	}
 
 	cfg := struct {
 		config.UIClient
