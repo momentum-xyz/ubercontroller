7a2d812f528b114b385fa65f316413234d9edd55
Boyan Donchev <bd@odyssey.org>
2022-12-06 16:58:09 +0200

    add struct db tags

--- types/entry/user_space.go
+++ types/entry/user_space.go
@@ -4,14 +4,20 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+
+	"github.com/momentum-xyz/ubercontroller/utils"
 )
 
 type UserSpace struct {
-	SpaceID   uuid.UUID
-	UserID    uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Value     UserSpaceValue
+	SpaceID   uuid.UUID      `db:"space_id"`
+	UserID    uuid.UUID      `db:"user_id"`
+	CreatedAt time.Time      `db:"created_at"`
+	UpdatedAt time.Time      `db:"updated_at"`
+	Value     UserSpaceValue `db:"value"`
 }
 
 type UserSpaceValue map[string]any
+
+func NewUserSpaceValue() *UserSpaceValue {
+	return utils.GetPTR(UserSpaceValue(make(map[string]any)))
+}
