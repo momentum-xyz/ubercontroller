99ac0e732ea24543e71c161539e1fdcf54c452d4
Dmitry Zinovyev <terbooter@gmail.com>
2022-12-07 17:15:35 +0300

    /newsfeed WIP: Convert demo data from string to structs

--- universe/node/api_timeline.go
+++ universe/node/api_timeline.go
@@ -1,15 +1,15 @@
 package node
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func (n *Node) apiNewsFeed(c *gin.Context) {
-	//TODO
-	jsonStr := `[
+const jsonStr = `[
   {
     "id": 1,
     "collectionId": 1,
@@ -243,8 +243,33 @@ func (n *Node) apiNewsFeed(c *gin.Context) {
   }
 ]
     `
-	c.DataFromReader(http.StatusOK,
-		int64(len(jsonStr)), gin.MIMEJSON, strings.NewReader(jsonStr), nil)
+
+type UserItem struct {
+	Id           int       `json:"id"`
+	CollectionId int       `json:"collectionId"`
+	Uuid         string    `json:"uuid"`
+	Owner        string    `json:"owner"`
+	Name         string    `json:"name"`
+	Description  string    `json:"description"`
+	Image        string    `json:"image"`
+	Date         time.Time `json:"date"`
+	Type         string    `json:"type"`
+}
+type FeedItem struct {
+	UserItem
+	ConnectedTo *UserItem `json:"connectedTo,omitempty"`
+	DockedTo    *UserItem `json:"dockedTo,omitempty"`
+}
+
+func (n *Node) apiNewsFeed(c *gin.Context) {
+
+	list := make([]FeedItem, 0)
+
+	json.Unmarshal([]byte(jsonStr), &list)
+
+	//TODO
+
+	c.JSON(http.StatusOK, list)
 }
 
 func (n *Node) apiNotifications(c *gin.Context) {
