39c297f10673d31606c746a3cb0edf9bc7f5ea18
Jorrit <js@odyssey.org>
2022-12-08 18:01:04 +0100

    fix db migration
    420 renamed this

--- database/migrations/sql/0000421_init_schema.up.sql
+++ database/migrations/sql/0000421_init_schema.up.sql
@@ -1,10 +1,10 @@
-INSERT INTO attribute
+INSERT INTO attribute_type
 (plugin_id, attribute_name, description)
 VALUES ('F0F0F0F0-0F0F-4FF0-AF0F-F0F0F0F0F0F0', 'world_settings', '')
 ON CONFLICT (plugin_id,attribute_name)
     DO UPDATE SET description = excluded.description;
 
-INSERT INTO attribute
+INSERT INTO attribute_type
 (plugin_id, attribute_name, description)
 VALUES ('F0F0F0F0-0F0F-4FF0-AF0F-F0F0F0F0F0F0', 'world_meta', '')
 ON CONFLICT (plugin_id,attribute_name)
