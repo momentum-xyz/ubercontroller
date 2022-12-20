7ab611e9cbd29d8c47bc9ef999d971bd20dc3451
Ramses Dijkstra <ramses@sigbar.nl>
2022-12-20 10:16:38 +0100

    ALP-32 Removed unused config params

--- config/common.go
+++ config/common.go
@@ -2,7 +2,6 @@ package config
 
 type Common struct {
 	AgoraAppCertificate string `yaml:"agora_app_certificate" envconfig:"AGORA_APP_CERTIFICATE"`
-	RenderDefaultURL    string `yaml:"render_default_url" envconfig:"RENDER_DEFAULT_URL"`
 	RenderInternalURL   string `yaml:"render_internal_url" envconfig:"RENDER_INTERNAL_URL"`
 	MnemonicPhrase      string `yaml:"mnemonic_phrase" envconfig:"MNEMONIC_PHRASE"`
 }
