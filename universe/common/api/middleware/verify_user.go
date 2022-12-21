a4aa637bb5c80a4cdd1e4224f52a7e3b3569c7e4
Ramses Dijkstra <ramses@sigbar.nl>
2022-12-21 14:45:32 +0100

    Simplified verifyUser

--- universe/common/api/middleware/verify_user.go
+++ universe/common/api/middleware/verify_user.go
@@ -7,23 +7,25 @@ import (
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 
-	"github.com/momentum-xyz/ubercontroller/types/generic"
-	"github.com/momentum-xyz/ubercontroller/universe"
 	"github.com/momentum-xyz/ubercontroller/universe/common/api"
 )
 
-var secretStore = generic.NewSyncMap[string, *[]byte](0)
+var secret *[]byte = nil
 
 func VerifyUser(log *zap.SugaredLogger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		secret, err := GetJWTSecret()
+	if secret == nil {
+		jwtSecret, err := api.GetJWTSecret()
 		if err != nil {
-			err = errors.WithMessage(err, "Middleware: VerifyUser: failed to get jwt secret")
-			api.AbortRequest(c, http.StatusInternalServerError, "failed_to_get_secret", err, log)
-			return
+			err = errors.WithMessage(err, "failed to fetch jwt secret")
+			log.Error(err)
+			return nil
 		}
 
-		token, err := api.ValidateJWTWithSecret(api.GetTokenFromRequest(c), secret)
+		secret = &jwtSecret
+	}
+
+	return func(c *gin.Context) {
+		token, err := api.ValidateJWTWithSecret(api.GetTokenFromRequest(c), *secret)
 		if err != nil {
 			err = errors.WithMessage(err, "Middleware: VerifyUser: failed to verify token")
 			api.AbortRequest(c, http.StatusForbidden, "failed_to_verify_access_token", err, log)
@@ -31,23 +33,4 @@ func VerifyUser(log *zap.SugaredLogger) gin.HandlerFunc {
 		}
 		c.Set(api.TokenContextKey, *token)
 	}
-}
-
-func GetJWTSecret() ([]byte, error) {
-	var secret []byte
-
-	store, ok := secretStore.Load(universe.Attributes.Node.JWTKey.Key)
-	if !ok {
-		jwtSecret, err := api.GetJWTSecret()
-		if err != nil {
-			return nil, errors.New("failed to get jwt secret")
-		}
-
-		secretStore.Store(universe.Attributes.Node.JWTKey.Key, &jwtSecret)
-		secret = jwtSecret
-	} else {
-		secret = *store
-	}
-
-	return secret, nil
 }
