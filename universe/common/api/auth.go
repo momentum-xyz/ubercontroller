760e8b0daec4bdf774701971a49a58244dfc8e36
Ramses Dijkstra <72547176+RamsesDijkstra@users.noreply.github.com>
2022-12-22 11:17:26 +0100

    feature/alp-32/verify-user-middleware(2) (#49)
    * Token refresh extended, PR fixes

--- universe/common/api/auth.go
+++ universe/common/api/auth.go
@@ -3,6 +3,7 @@ package api
 import (
 	"bytes"
 	"fmt"
+
 	"strings"
 	"time"
 
@@ -10,8 +11,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
-	"github.com/momentum-xyz/ubercontroller/utils"
 	"github.com/pkg/errors"
+
+	"github.com/momentum-xyz/ubercontroller/types/entry"
+	"github.com/momentum-xyz/ubercontroller/universe"
+	"github.com/momentum-xyz/ubercontroller/utils"
 )
 
 func GetTokenFromRequest(c *gin.Context) string {
@@ -80,17 +84,21 @@ func VerifyPolkadotSignature(wallet, challenge, signature string) (bool, error) 
 
 // CreateJWTToken saves a jwt token with the given userID as subject
 // and signed with the given secret
-func CreateJWTToken(userID uuid.UUID, secret []byte) (string, error) {
+func CreateJWTToken(userID uuid.UUID) (string, error) {
 	claims := jwt.StandardClaims{
 		IssuedAt:  time.Now().Unix(),
-		ExpiresAt: time.Now().Add(4 * time.Hour).Unix(),
+		ExpiresAt: time.Now().Add(7 * 24 * time.Hour).Unix(),
 		Issuer:    "ubercontroller",
 		Subject:   userID.String(),
 	}
 
-	jwt := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	newJwt := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret, err := GetJWTSecret()
+	if err != nil {
+		return "", errors.WithMessage(err, "failed to get jwt secret")
+	}
 
-	signedString, err := jwt.SignedString(secret)
+	signedString, err := newJwt.SignedString(secret)
 	if err != nil {
 		return "", errors.WithMessage(err, "failed to sign token")
 	}
@@ -98,16 +106,23 @@ func CreateJWTToken(userID uuid.UUID, secret []byte) (string, error) {
 	return signedString, nil
 }
 
-func ValidateJWT(signedString string, secret []byte) (*jwt.Token, error) {
-	parser := new(jwt.Parser)
-	token, _, err := parser.ParseUnverified(signedString, jwt.MapClaims{})
-	return token, err
-	/* TODO:
+func GetJWTSecret() ([]byte, error) {
+	jwtSecret, ok := universe.GetNode().GetNodeAttributeValue(
+		entry.NewAttributeID(universe.GetSystemPluginID(), universe.Attributes.Node.JWTKey.Name),
+	)
+	if !ok || jwtSecret == nil {
+		return nil, errors.New("failed to get jwt secret")
+	}
+	secret := utils.GetFromAnyMap(*jwtSecret, universe.Attributes.Node.JWTKey.Key, "")
+
+	return []byte(secret), nil
+}
+
+func ValidateJWTWithSecret(signedString string, secret []byte) (*jwt.Token, error) {
 	return jwt.Parse(signedString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Invalid token %v", token.Header["alg"])
 		}
 		return secret, nil
 	})
-	*/
 }
--- universe/common/api/middleware/verify_user.go
+++ universe/common/api/middleware/verify_user.go
@@ -7,27 +7,25 @@ import (
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 
-	"github.com/momentum-xyz/ubercontroller/types/entry"
-	"github.com/momentum-xyz/ubercontroller/universe"
 	"github.com/momentum-xyz/ubercontroller/universe/common/api"
-	"github.com/momentum-xyz/ubercontroller/utils"
 )
 
 func VerifyUser(log *zap.SugaredLogger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// get jwt secret
-		jwtSecret, ok := universe.GetNode().GetNodeAttributeValue(
-			entry.NewAttributeID(universe.GetSystemPluginID(), universe.Attributes.Node.JWTKey.Name),
-		)
-		if !ok || jwtSecret == nil {
-			err := errors.New("Middleware: VerifyUser: failed to get jwt_key attribute")
-			api.AbortRequest(c, http.StatusInternalServerError, "failed_to_get_jwt_key", err, log)
-			return
-		}
-		secret := utils.GetFromAnyMap(*jwtSecret, "secret", "")
+	var secret []byte
 
-		// auth
-		token, err := api.ValidateJWT(api.GetTokenFromRequest(c), []byte(secret))
+	return func(c *gin.Context) {
+		if secret == nil {
+			jwtSecret, err := api.GetJWTSecret()
+			if err != nil {
+				err = errors.WithMessage(err, "Middleware: VerifyUser: failed to fetch jwt secret")
+				api.AbortRequest(c, http.StatusForbidden, "failed_to_verify_access_token", err, log)
+				return
+			}
+
+			secret = jwtSecret
+		}
+
+		token, err := api.ValidateJWTWithSecret(api.GetTokenFromRequest(c), secret)
 		if err != nil {
 			err = errors.WithMessage(err, "Middleware: VerifyUser: failed to verify token")
 			api.AbortRequest(c, http.StatusForbidden, "failed_to_verify_access_token", err, log)
