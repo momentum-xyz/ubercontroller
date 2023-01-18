db4bb02aeafcc57cd08556b4ac398ad2a3fb3813
Jorrit <js@odyssey.org>
2023-01-18 10:55:59 +0100

    ui-client unity config
    Allow changing the unity client that is used.
    The domain it is hosted on and the file names.
    
    Can be used to work with a locally deployed/build unity.
    And needed to migrate the default filenames at some point.

--- config/uiclient.go
+++ config/uiclient.go
@@ -7,10 +7,15 @@ type UIClient struct {
 	NFTAdminAddress               string `yaml:"nft_admin_address" json:"NFT_ADMIN_ADDRESS" envconfig:"NFT_ADMIN_ADDRESS"`
 	NFTCollectionOdysseyID        string `yaml:"nft_collection_odyssey_id" json:"NFT_COLLECTION_ODYSSEY_ID" envconfig:"NFT_COLLECTION_ODYSSEY_ID"`
 	StreamchatKey                 string `yaml:"streamchat_key" json:"STREAMCHAT_KEY" envconfig:"STREAMCHAT_KEY"`
-	UnityClientStreamingAssetsURL string `yaml:"unity_client_streaming_assets_url" json:"UNITY_CLIENT_STREAMING_ASSETS_URL" envconfig:"UNITY_CLIENT_STREAMING_ASSETS_URL"`
 	UnityClientCompanyName        string `yaml:"unity_client_company_name" json:"UNITY_CLIENT_COMPANY_NAME" envconfig:"UNITY_CLIENT_COMPANY_NAME"`
 	UnityClientProductName        string `yaml:"unity_client_product_name" json:"UNITY_CLIENT_PRODUCT_NAME" envconfig:"UNITY_CLIENT_PRODUCT_NAME"`
 	UnityClientProductVersion     string `yaml:"unity_client_product_version" json:"UNITY_CLIENT_PRODUCT_VERSION" envconfig:"UNITY_CLIENT_PRODUCT_VERSION"`
+	UnityClientStreamingAssetsURL string `yaml:"unity_client_streaming_assets_url" json:"UNITY_CLIENT_STREAMING_ASSETS_URL" envconfig:"UNITY_CLIENT_STREAMING_ASSETS_URL"`
+	UnityClientURL                string `yaml:"unity_client_url" json:"UNITY_CLIENT_URL" envconfig:"UNITY_CLIENT_URL"`
+	UnityLoaderFileName           string `yaml:"unity_loader_file_name" json:"UNITY_LOADER_FILE_NAME" envconfig:"UNITY_LOADER_FILE_NAME"`
+	UnityFrameworkFileName        string `yaml:"unity_framework_file_name" json:"UNITY_FRAMEWORK_FILE_NAME" envconfig:"UNITY_FRAMEWORK_FILE_NAME"`
+	UnityDataFileName             string `yaml:"unity_data_file_name" json:"UNITY_DATA_FILE_NAME" envconfig:"UNITY_DATA_FILE_NAME"`
+	UnityCodeFileName             string `yaml:"unity_code_file_name" json:"UNITY_CODE_FILE_NAME" envconfig:"UNITY_CODE_FILE_NAME"`
 }
 
 func (c *UIClient) Init() {
@@ -18,4 +23,8 @@ func (c *UIClient) Init() {
 	c.UnityClientCompanyName = "Odyssey"
 	c.UnityClientProductName = "Odyssey Momentum"
 	c.UnityClientProductVersion = "0.1"
+	c.UnityLoaderFileName = "WebGL.loader.js"
+	c.UnityFrameworkFileName = "WebGL.framework.js.gz"
+	c.UnityCodeFileName = "WebGL.wasm.gz"
+	c.UnityDataFileName = "WebGL.data.gz"
 }
