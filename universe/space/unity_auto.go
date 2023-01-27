15cb6d0f156aa461cedc26faf865605697a7e726
Anton Starikov <ant.starikov@gmail.com>
2023-01-27 11:19:48 +0100

    Test for early skybox texture send, part2

--- universe/space/unity_auto.go
+++ universe/space/unity_auto.go
@@ -123,7 +123,8 @@ func (s *Space) UpdateAutoTextureMap(
 			defer s.renderTextureMap.Mu.RUnlock()
 
 			s.textMsg.Store(message.GetBuilder().SetObjectTextures(s.GetID(), s.renderTextureMap.Data))
-			if s.GetSpaceType().GetAsset3d().GetID() == uuid.MustParse("313a597a-8b9a-47a7-9908-52bdc7a21a3e") {
+			st := s.GetSpaceType()
+			if st != nil && st.GetAsset3d() != nil && st.GetAsset3d().GetID() == uuid.MustParse("313a597a-8b9a-47a7-9908-52bdc7a21a3e") {
 				s.log.Debugf("Setting skybox texture for %+v to %+v\n", s.world.GetID(), val)
 				skyBoxTextureMap := map[string]string{option.SlotName: val}
 				s.world.TempSetSkybox(message.GetBuilder().SetObjectTextures(s.GetID(), skyBoxTextureMap))
