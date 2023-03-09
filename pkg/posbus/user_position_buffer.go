70291560454f3b00a1fb183022b3f695f4622c32
Anton Starikov <ant.starikov@gmail.com>
2023-03-09 13:47:11 +0100

    Fix typos in UserTransformBuffer.Decode

--- pkg/posbus/user_position_buffer.go
+++ pkg/posbus/user_position_buffer.go
@@ -41,24 +41,17 @@ func (utb *UserTransformBuffer) Buf() []byte {
 	return utb.posBuffer[:MsgArrTypeSize+utb.nUsers*UserTransformMessageSize]
 }
 
-func (utb *UserTransformBuffer) Decode() []struct {
-	ID        uuid.UUID
-	Transofrm cmath.UserTransform
-} {
+func (utb *UserTransformBuffer) Decode() []UserPosition {
 	t := make(
-		[]struct {
-			ID        uuid.UUID
-			Transofrm cmath.UserTransform
-		}, utb.nUsers,
+		[]UserPosition, utb.nUsers,
 	)
 	start := MsgArrTypeSize
 	for i := 0; i < utb.nUsers; i++ {
-		//t[i].ID=uuid.UUID(utb.posBuffer[start:start+16])
 		copy(t[i].ID[:], utb.posBuffer[start:start+16])
 		start += 16
-		t[i].Transofrm.Position = &cmath.Vec3{}
-		t[i].Transofrm.Rotation = &cmath.Vec3{}
-		t[i].Transofrm.CopyFromBuffer(utb.posBuffer[start:])
+		t[i].Transform.Position = &cmath.Vec3{}
+		t[i].Transform.Rotation = &cmath.Vec3{}
+		t[i].Transform.CopyFromBuffer(utb.posBuffer[start:])
 		start += UserTransformMessageSize
 	}
 	return t
