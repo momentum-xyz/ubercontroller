6813a50c1919c71d357f013745bd59b4d1a2366c
Anton Starikov <ant.starikov@gmail.com>
2023-03-22 12:16:35 +0100

    Adds TriggerVisualEffects message

---
+++ pkg/posbus/trigger_visual_effects.go
@@ -0,0 +1,19 @@
+package posbus
+
+// FIXME: finalize VisualEffect type with all data
+type VisualEffect struct {
+	Name string `json:"name"`
+}
+
+type TriggerVisualEffects struct {
+	Effects []VisualEffect `json:"effects"`
+}
+
+func init() {
+	registerMessage(TriggerVisualEffects{})
+	addExtraType(VisualEffect{})
+}
+
+func (g *TriggerVisualEffects) Type() MsgType {
+	return 0xD96089C6
+}
