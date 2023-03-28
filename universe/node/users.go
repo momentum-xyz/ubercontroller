f7cec59d8dcf8a0db5e8194c3be2a87b2223e817
Jorrit <js@odyssey.org>
2023-03-28 15:37:23 +0200

    feat: export user creation function from Node
    So user entries can be created outside of the node package.

--- universe/node/users.go
+++ universe/node/users.go
@@ -1,8 +1,10 @@
 package node
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/momentum-xyz/ubercontroller/types/entry"
 	"github.com/momentum-xyz/ubercontroller/utils/umid"
 
 	"github.com/pkg/errors"
@@ -26,4 +28,14 @@ func (n *Node) LoadUser(userID umid.UMID) (universe.User, error) {
 	user.SetPosition(cmath.Vec3{X: 50, Y: 50, Z: 150})
 	fmt.Printf("%+v\n", user.GetPosition())
 	return user, nil
+}
+
+// Create new users on this node.
+func (n *Node) CreateUsers(ctx context.Context, users ...*entry.User) error {
+	if err := n.db.GetUsersDB().UpsertUsers(ctx, users); err != nil {
+		return errors.Wrap(err, "create users")
+	}
+	// TODO: add 'returning` to the query, so we can give back user objects
+	// with their filled in database defaults and ID.
+	return nil
 }
