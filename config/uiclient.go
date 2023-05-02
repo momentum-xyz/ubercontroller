a4bc719610e90be99ce9a1435cbeb9958015d81e
Jorrit <js@odyssey.org>
2023-05-02 18:06:00 +0200

    feat(ui-conf): blockchain id for FE

--- config/uiclient.go
+++ config/uiclient.go
@@ -3,6 +3,7 @@ package config
 type UIClient struct {
 	AgoraAppID             string `yaml:"agora_app_id" json:"AGORA_APP_ID" envconfig:"AGORA_APP_ID"`
 	BlockchainWsServer     string `yaml:"blockchain_ws_server" json:"BLOCKCHAIN_WS_SERVER" envconfig:"BLOCKCHAIN_WS_SERVER"`
+	BlockchainID           string `yaml:"arbitrum_chain_id" json:"BLOCKCHAIN_ID" envconfig:"ARBITRUM_CHAIN_ID"`
 	FrontendURL            string `yaml:"frontend_url" json:"-" envconfig:"FRONTEND_URL"`
 	NFTAdminAddress        string `yaml:"nft_admin_address" json:"NFT_ADMIN_ADDRESS" envconfig:"NFT_ADMIN_ADDRESS"`
 	NFTCollectionOdysseyID string `yaml:"nft_collection_odyssey_id" json:"NFT_COLLECTION_ODYSSEY_ID" envconfig:"NFT_COLLECTION_ODYSSEY_ID"`
