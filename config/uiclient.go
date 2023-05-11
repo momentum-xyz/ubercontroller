3d9bdb43dcbc6f2cea988ea6d84a30863b37a942
Jorrit <js@odyssey.org>
2023-05-11 17:01:03 +0200

    config(ui-client): blockchain ones aren't overridable in yaml
    envconfig was already removed. These are json 'output' only.

--- config/uiclient.go
+++ config/uiclient.go
@@ -3,12 +3,12 @@ package config
 type UIClient struct {
 	AgoraAppID             string `yaml:"agora_app_id" json:"AGORA_APP_ID" envconfig:"AGORA_APP_ID"`
 	BlockchainWsServer     string `yaml:"blockchain_ws_server" json:"BLOCKCHAIN_WS_SERVER" envconfig:"BLOCKCHAIN_WS_SERVER"`
-	BlockchainID           string `yaml:"arbitrum_chain_id" json:"BLOCKCHAIN_ID"`
-	ContractDAD            string `yaml:"arbitrum_dad_token_address" json:"CONTRACT_DAD_ADDRESS"`
-	ContractFaucet         string `yaml:"arbitrum_faucet_contract_address" json:"CONTRACT_FAUCET_ADDRESS"`
-	ContractMOM            string `yaml:"arbitrum_mom_token_address" json:"CONTRACT_MOM_ADDRESS"`
-	ContractNFT            string `yaml:"arbitrum_nft_contract_address" json:"CONTRACT_NFT_ADDRESS"`
-	ContractStake          string `yaml:"arbitrum_stake_token_address" json:"CONTRACT_STAKING_ADDRESS"`
+	BlockchainID           string `json:"BLOCKCHAIN_ID"`
+	ContractDAD            string `json:"CONTRACT_DAD_ADDRESS"`
+	ContractFaucet         string `json:"CONTRACT_FAUCET_ADDRESS"`
+	ContractMOM            string `json:"CONTRACT_MOM_ADDRESS"`
+	ContractNFT            string `json:"CONTRACT_NFT_ADDRESS"`
+	ContractStake          string `json:"CONTRACT_STAKING_ADDRESS"`
 	FrontendURL            string `yaml:"frontend_url" json:"-" envconfig:"FRONTEND_URL"`
 	NFTAdminAddress        string `yaml:"nft_admin_address" json:"NFT_ADMIN_ADDRESS" envconfig:"NFT_ADMIN_ADDRESS"`
 	NFTCollectionOdysseyID string `yaml:"nft_collection_odyssey_id" json:"NFT_COLLECTION_ODYSSEY_ID" envconfig:"NFT_COLLECTION_ODYSSEY_ID"`
