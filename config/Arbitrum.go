4a95cc9b69fbc49c1b18308d6df8f62e84d57416
Jorrit <js@odyssey.org>
2023-05-11 16:56:16 +0200

    fix: add a default faucet contract address
    To get a functional local development env.

--- config/Arbitrum.go
+++ config/Arbitrum.go
@@ -18,6 +18,7 @@ func (a *Arbitrum) Init() {
 	a.DADTokenAddress = "0x0244BbA6fcB25eFed05955C4A1B86A458986D2e0"
 	a.StakeAddress = "0x047C0A154271498ee718162b718b3D4F464855e0"
 	a.NFTAddress = "0x97E0B10D89a494Eb5cfFCc72853FB0750BD64AcD"
+	a.FaucetAddress = "0x9E760F1CddA0694B6156076C60657118CF874289"
 	a.RPCURL = "https://bcdev.antst.net:8547"
 	a.WSURL = "wss://bcdev.antst.net:8548"
 }
