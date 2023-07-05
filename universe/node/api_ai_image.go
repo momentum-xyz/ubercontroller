88fa70d0cb8f0bf921c0f1e4a49799f140dd7776
Ramses Dijkstra <ramses@sigbar.nl>
2023-07-05 14:57:26 +0200

    Get images by leonrdoID EP

--- universe/node/api_ai_image.go
+++ universe/node/api_ai_image.go
@@ -3,7 +3,6 @@ package node
 import (
 	"bytes"
 	"encoding/json"
-	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -21,6 +20,104 @@ type LeonardoResponse struct {
 	} `json:"sdGenerationJob"`
 }
 
+type GeneratedImage struct {
+	URL       string `json:"url"`
+	NSFW      bool   `json:"nsfw"`
+	ID        string `json:"id"`
+	LikeCount int    `json:"likeCount"`
+}
+
+type GenerationResponse struct {
+	GenerationsByPK struct {
+		GeneratedImages []GeneratedImage `json:"generated_images"`
+		ModelID         string           `json:"modelId"`
+		Prompt          string           `json:"prompt"`
+		NegativePrompt  string           `json:"negativePrompt"`
+		ImageHeight     int              `json:"imageHeight"`
+		ImageWidth      int              `json:"imageWidth"`
+		InferenceSteps  int              `json:"inferenceSteps"`
+		Seed            int              `json:"seed"`
+		Public          bool             `json:"public"`
+		Scheduler       string           `json:"scheduler"`
+		SdVersion       string           `json:"sdVersion"`
+		Status          string           `json:"status"`
+		PresetStyle     *string          `json:"presetStyle"`
+		InitStrength    *float64         `json:"initStrength"`
+		GuidanceScale   int              `json:"guidanceScale"`
+		ID              string           `json:"id"`
+		CreatedAt       string           `json:"createdAt"`
+	} `json:"generations_by_pk"`
+}
+
+// @Summary Get images be generation id
+// @Schemes
+// @Description Returns an array of images by generation id
+// @Tags ai-images
+// @Accept json
+// @Produce json
+// @Param leonardoID path string true "LeonardoID string"
+// @Success 200 {object} node.apiPostSkyboxGenerate.Out
+// @Failure 400 {object} api.HTTPError
+// @Failure 500 {object} api.HTTPError
+// @Router /api/v4/ai-image/generate/{leonardo_id} [get]
+func (n *Node) apiGetImageGeneration(c *gin.Context) {
+	leonardoID := c.Param("leonardoID")
+
+	userID, err := api.GetUserIDFromContext(c)
+	if err != nil {
+		err := errors.WithMessage(err, "Node: apiGetImageGeneration: failed to get user umid from context")
+		api.AbortRequest(c, http.StatusInternalServerError, "get_user_id_failed", err, n.log)
+		return
+	}
+	_ = userID
+
+	apiKey, err := n.getApiLeonardoKeyAndSecret()
+	if err != nil {
+		err := errors.WithMessage(err, "Node: apiGetImageGeneration: failed to getApiKeyAndSecret")
+		api.AbortRequest(c, http.StatusNotFound, "node_attribute_not_found", err, n.log)
+		return
+	}
+
+	apiUrl := "https://cloud.leonardo.ai/api/rest/v1/generations/" + leonardoID
+
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	if err != nil {
+		err := errors.WithMessage(err, "Node: apiGetImageGeneration: failed to create new request to blockadelabs API")
+		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", err, n.log)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+*apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	r, err := client.Do(req)
+	if err != nil {
+		err := errors.WithMessage(err, "Node: apiGetImageGeneration: failed to send post request to blockadelabs API")
+		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", err, n.log)
+		return
+	}
+
+	response := GenerationResponse{}
+	if err := json.NewDecoder(r.Body).Decode(&response); err != nil {
+		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", errors.New("Node: apiGetImageGeneration: failed to decode leonardo API response"), n.log)
+		return
+	}
+
+	r.Body.Close()
+
+	type Out struct {
+		Success bool               `json:"success"`
+		Data    GenerationResponse `json:"data"`
+	}
+	out := Out{
+		Success: true,
+		Data:    response,
+	}
+
+	c.JSON(http.StatusOK, out)
+}
+
 // @Summary Create a Generation of Images
 // @Schemes
 // @Description Returns a generation id with which the images can be fetched
@@ -32,7 +129,7 @@ type LeonardoResponse struct {
 // @Failure 400 {object} api.HTTPError
 // @Failure 500 {object} api.HTTPError
 // @Router /api/v4/ai-image/generate [post]
-func (n *Node) apiPostGetImageGenerationID(c *gin.Context) {
+func (n *Node) apiPostImageGenerationID(c *gin.Context) {
 	type Body struct {
 		Prompt string `json:"prompt" binding:"required"`
 	}
@@ -91,23 +188,14 @@ func (n *Node) apiPostGetImageGenerationID(c *gin.Context) {
 		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", err, n.log)
 		return
 	}
-	defer r.Body.Close()
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		err := errors.New("Node: apiPostGetImageGenerationID: failed to read leonardo API response")
-		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", err, n.log)
-		return
-	}
 
 	response := LeonardoResponse{}
-
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		err := errors.New("Node: apiPostGetImageGenerationID: failed to Unmarshal leonardo API response")
-		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", err, n.log)
+	if err := json.NewDecoder(r.Body).Decode(&response); err != nil {
+		api.AbortRequest(c, http.StatusInternalServerError, "internal_error", errors.New("Node: apiPostGetImageGenerationID: failed to decode leonardo API response"), n.log)
 		return
 	}
+
+	r.Body.Close()
 
 	type Out struct {
 		Success bool             `json:"success"`
