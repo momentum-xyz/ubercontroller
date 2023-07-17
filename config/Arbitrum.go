b9066c7d34be21d7a4da3f180432ba76bd8430bd
Jorrit <js@odyssey.org>
2023-07-17 12:42:03 +0200

    chore: update default contract addresses
    Update them to latest stuff on private testnet.

--- config/Arbitrum.go
+++ config/Arbitrum.go
@@ -14,10 +14,10 @@ type Arbitrum struct {
 func (a *Arbitrum) Init() {
 	// Default values connect to a private testnet.
 	a.BlockchainID = "412346"
-	a.MOMTokenAddress = "0x0147a5cB13Ab1D75f09385e53118AD00D4B74778"
-	a.DADTokenAddress = "0x37EcF9B8A0fAceF220f2Bd3C7C30Cbf416433564"
-	a.StakeAddress = "0xBCb202a9B77d9A44B54e30f7adF30d9eBd4Cd145"
-	a.NFTAddress = "0x9f89a52a29A9964DE6B2450b8edA92Ad2d2ba146"
+	a.MOMTokenAddress = "0x457fd0Ee3Ce35113ee414994f37eE38518d6E7Ee"
+	a.DADTokenAddress = "0xfCa1B6bD67AeF9a9E7047bf7D3949f40E8dde18d"
+	a.StakeAddress = "0x18f3FEE919DBc22b5a68401298B01dcd46ab8665"
+	a.NFTAddress = "0xbc48cb82903f537614E0309CaF6fe8cEeBa3d174"
 	a.FaucetAddress = "0x9E760F1CddA0694B6156076C60657118CF874289"
 	a.RPCURL = "https://bcdev.antst.net:8547"
 	a.WSURL = "wss://bcdev.antst.net:8548"
