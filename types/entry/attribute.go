momentum-xyz/ubercontroller

commit 9b96d28c9d69737d95d03a9a49c3e60479a4648f

fix: render_auto value_field setting
Properly use the field name from the options.

universe/object/render_auto.go
@@ -104,10 +104,7 @@ func (o *Object) UpdateAutoTextureMap(
 
 		data = val
 	case entry.SlotTypeTexture:
-		valField := "auto_render_hash"
-		if option.ContentType == "image" {
-			valField = "render_hash"
-		}
+		valField := option.ValueField // set in GetOptionAutoOption
 		v, ok := (*value)[valField]
 		if !ok {
 			return nil

database/migrations/sql/000686_fix_usr_cstm_attr.down.sql
@@ -1,0 +1,16 @@
+BEGIN;
+
+INSERT INTO attribute_type (plugin_id, attribute_name, description, options)
+VALUES ('f0f0f0f0-0f0f-4ff0-af0f-f0f0f0f0f0f0',
+        'user_customisable_data',
+        'Data for user customisable objects',
+        '{
+            "render_auto": {
+                "slot_type": "texture",
+                "content_type": "image"
+            }
+        }')
+    ON CONFLICT (plugin_id, attribute_name) DO UPDATE
+    SET description=EXCLUDED.description, options=EXCLUDED.options
+;
+COMMIT;

database/migrations/sql/000686_fix_usr_cstm_attr.up.sql
@@ -1,0 +1,18 @@
+BEGIN;
+
+INSERT INTO attribute_type (plugin_id, attribute_name, description, options)
+VALUES ('f0f0f0f0-0f0f-4ff0-af0f-f0f0f0f0f0f0',
+        'user_customisable_data',
+        'Data for user customisable objects',
+        '{
+            "render_auto": {
+                "slot_type": "texture",
+                "content_type": "image",
+				"slot_name":    "object_texture",
+				"value_field":  "image_hash"
+            }
+        }')
+    ON CONFLICT (plugin_id, attribute_name) DO UPDATE
+    SET description=EXCLUDED.description, options=EXCLUDED.options
+;
+COMMIT;

seed/attribute_type.go
@@ -471,6 +471,8 @@ func seedAttributeType(ctx context.Context, node universe.Node) error {
 				"render_auto": map[string]string{
 					"slot_type":    "texture",
 					"content_type": "image",
+					"slot_name":    "object_texture",
+					"value_field":  "image_hash",
 				},
 			},
 		},

commit 498786c9d19c6c81afb771664790630318f759c3

fix: auto render name
Broke in 9b96d28c9d69737d95d03a9a49c3e60479a4648f

Bring back the hardcoded value field.

types/entry/attribute.go
@@ -66,6 +66,8 @@ type RenderAutoAttributeOption struct {
 	TextRenderTemplate string          `db:"text_render_template" json:"text_render_template"`
 }
 
+const RenderAutoHashFieldName = "auto_render_hash"
+
 type PermissionsRoleType string
 
 const (

universe/object/render_auto.go
@@ -41,7 +41,7 @@ func (o *Object) renderAutoOnObjectAttributeChanged(
 			o.objectAttributes.object.Mu.Lock()
 			defer o.objectAttributes.object.Mu.Unlock()
 
-			(*value)["auto_render_hash"] = hash.Hash
+			(*value)[entry.RenderAutoHashFieldName] = hash.Hash
 
 			if err := o.db.GetObjectAttributesDB().UpdateObjectAttributeValue(
 				o.ctx, entry.NewObjectAttributeID(attributeID, o.GetID()), value,
@@ -104,7 +104,14 @@ func (o *Object) UpdateAutoTextureMap(
 
 		data = val
 	case entry.SlotTypeTexture:
-		valField := option.ValueField // set in GetOptionAutoOption
+		var valField string
+		if option.ContentType == "text" || option.ContentType == "video" {
+			// These are 'prerendered', see ~slot.PrerenderAutoValue
+			// and the TODO comment above where it is set.
+			valField = entry.RenderAutoHashFieldName
+		} else {
+			valField = option.ValueField
+		}
 		v, ok := (*value)[valField]
 		if !ok {
 			return nil