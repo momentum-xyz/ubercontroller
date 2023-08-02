56176582fd3e89e84c72452ec3e005d150742fd9
Ramses Dijkstra <rd@odyssey.org>
2023-08-02 16:00:55 +0200

    Fixed migrations

--- database/migrations/sql/000695_update_object_color_state_attribute_type_options.down.sql
+++ database/migrations/sql/000695_update_object_color_state_attribute_type_options.down.sql
@@ -6,6 +6,6 @@ WHERE attribute_name = 'object_color' AND jsonb_exists(options, 'cloneable');
 
 UPDATE attribute_type
 SET options = options #- '{cloneable}'
-WHERE attribute_name = 'state' AND jsonb_exists(options, 'cloneable');
+WHERE attribute_name = 'state' AND jsonb_exists(options, 'cloneable') AND plugin_id = 'ff40fbf0-8c22-437d-b27a-0258f99130fe';
 
 COMMIT;
--- database/migrations/sql/000695_update_object_color_state_attribute_type_options.up.sql
+++ database/migrations/sql/000695_update_object_color_state_attribute_type_options.up.sql
@@ -6,6 +6,6 @@ WHERE attribute_name = 'object_color' AND NOT jsonb_exists(options, 'cloneable')
 
 UPDATE attribute_type
 SET options = options || '{"cloneable": {}}'::jsonb
-WHERE attribute_name = 'state' AND NOT jsonb_exists(options, 'cloneable');
+WHERE attribute_name = 'state' AND plugin_id = 'ff40fbf0-8c22-437d-b27a-0258f99130fe' AND NOT jsonb_exists(options, 'cloneable');
 
 COMMIT;
