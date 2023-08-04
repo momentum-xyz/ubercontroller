4769919840f4d898175599a66474e00da840ea33
Jorrit <js@odyssey.org>
2023-08-04 13:35:00 +0200

    feat: ui config for mint/creating new odyssey nfts
    default values are just placeholders, no actually working backend for
    this yet.

--- config/Arbitrum.go
+++ config/Arbitrum.go
@@ -6,6 +6,8 @@ type Arbitrum struct {
 	DADTokenAddress string `yaml:"arbitrum_dad_token_address" envconfig:"ARBITRUM_DAD_TOKEN_ADDRESS"`
 	StakeAddress    string `yaml:"arbitrum_stake_token_address" envconfig:"ARBITRUM_STAKE_ADDRESS"`
 	NFTAddress      string `yaml:"arbitrum_nft_address" envconfig:"ARBITRUM_NFT_ADDRESS"`
+	MintNFTAmount   string `json:"arbitrum_mint_nft_amount" envconfig:"ARBITRUM_MINT_NFT_AMOUNT"`
+	MintNFTDeposit  string `json:"arbitrum_mint_nft_deposit_address" envconfig:"ARBITRUM_MINT_NFT_DEPOSIT_ADDRESS"`
 	FaucetAddress   string `yaml:"arbitrum_faucet_address" envconfig:"ARBITRUM_FAUCET_ADDRESS"`
 	RPCURL          string `yaml:"arbitrum_rpc_url" envconfig:"ARBITRUM_RPC_URL"`
 	WSURL           string `yaml:"arbitrum_ws_url" envconfig:"ARBITRUM_WS_URL"`
@@ -19,6 +21,8 @@ func (a *Arbitrum) Init() {
 	a.StakeAddress = "0x18f3FEE919DBc22b5a68401298B01dcd46ab8665"
 	a.NFTAddress = "0xbc48cb82903f537614E0309CaF6fe8cEeBa3d174"
 	a.FaucetAddress = "0x9E760F1CddA0694B6156076C60657118CF874289"
+	a.MintNFTAmount = "4.20"
+	a.MintNFTDeposit = "0x683642c22feDE752415D4793832Ab75EFdF6223c"
 	a.RPCURL = "https://bcdev.antst.net:8547"
 	a.WSURL = "wss://bcdev.antst.net:8548"
 }
