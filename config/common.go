3cac31de5608f56b333a4690663d685013bcc195
Jorrit <js@odyssey.org>
2023-08-31 14:17:02 +0200

    fixup! chore: clenaup unused drive nft bits
    Remove unused config var.

--- config/common.go
+++ config/common.go
@@ -2,7 +2,6 @@ package config
 
 type Common struct {
 	AgoraAppCertificate string `yaml:"agora_app_certificate" envconfig:"AGORA_APP_CERTIFICATE"`
-	MnemonicPhrase      string `yaml:"mnemonic_phrase" envconfig:"MNEMONIC_PHRASE"`
 	AllowCORS           bool   `yaml:"allow_cors" envconfig:"ALLOW_CORS"`
 
 	// Enabled pprof http endpoints. If enabled, point your browser at /debug/pprof
