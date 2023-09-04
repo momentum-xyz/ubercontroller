e63127c7f6c0830fbf5221863c7414ca9ede3ad2
Jorrit <js@odyssey.org>
2023-09-04 10:14:09 +0200

    feat: optionally allow serving the UI/frontend files
    Point FRONTEND_SERVE_DIR to a directory with the build ui-client
    package.
    
    Allows hosting it as a single process.

--- config/local.go
+++ config/local.go
@@ -6,6 +6,7 @@ type Local struct {
 	LogLevel         int    `yaml:"loglevel"  envconfig:"UBERCONTROLLER_LOGLEVEL"`
 	ExtensionStorage string `yaml:"storage"  envconfig:"UBERCONTROLLER_STORAGE"`
 	SeedDataFiles    string `yaml:"seed_data_files" envconfig:"UBERCONTROLLER_SEED_DATA_FILES"`
+	FrontendServeDir string `yaml:"frontend_serve_dir" envconfig:"FRONTEND_SERVE_DIR"`
 	// TODO: rename FrontendURL to avoid confusing this with 'the frontend'
 	FrontendURL string `yaml:"frontend_url" json:"-" envconfig:"FRONTEND_URL"` // URL where this instance is reachable (e.g. when behind a proxy)
 }
